92e03f324bae025a3aeb11e6246e994ebe4a3595
Tim Renken <quiptobasco@gmail.com>
2024-03-20 22:14:57 -0700

    first

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        profilePic: {
+            type: String,
+            default: '',
+        },
+        // createdAt, updatedAt => Member since <createdAt>
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
